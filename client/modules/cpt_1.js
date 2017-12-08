d12592c8ced57e9277171d05b823782e5f24700d
guohjia <guohjia@gmail.com>
2017-12-09 01:25:17 +0800

    add background cover and contain

--- client/modules/cpt_1.js
+++ client/modules/cpt_1.js
@@ -11,10 +11,19 @@ export default class CPT_1 extends React.Component{
     render(){
         return (
             <div className={Style.test}>
-                <span>这张图片大于8k,file-loader返回编码后的路径直接发请求处理图片</span>
-                <div className="img img_test1"></div>
-                <span>这张图片小于8k,url-loader处理,base64,打包进入css,节约请求,但是css文件会变大</span>
-                <div className="img img_test2"></div>
+                <span>background-contain</span>
+                <p>
+                    contain, on the other hand, says to always show the whole image,
+                    even if that leaves a little space to the sides or bottom.
+                    <br/>(显示整张图片即可(但也会尽量去填容器,先填满宽/高中小的一方面,另外一边自适应),即使宽度/高度没填满,也可以多填一张)
+                </p>
+                <div className="imgWrap img_test1"></div>
+                <span>background-cover</span>
+                    <p>cover tells the browser to make sure the image always covers the entire container, 
+                        even if it has to stretch the image or cut a little bit off one of the edges.
+                        <br/>(宽高必须都铺满或超过容器宽高,先去填满容器宽高中的大的一方面,另外一方面自适应) 
+                    </p>
+                <div className="imgWrap img_test2"></div>
             </div>
         )
     }
