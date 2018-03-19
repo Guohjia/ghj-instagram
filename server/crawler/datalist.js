55a0ef35b42731b73e12063adb114219c3e4511e
guohjia <guohjia@gmail.com>
2018-03-19 23:09:05 +0800

    add get data

---
+++ server/crawler/datalist.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer')
+
+const url = 'https://movie.douban.com/tag/#/?sort=T&range=0,10&tags=%E7%94%B5%E5%BD%B1'
+
+const sleep = time => new Promise(resolve => {
+    setTimeout(resolve, time)
+});
+(async () => {
+    console.log('start visit target page');
+    const browser = await puppeteer.launch({
+        args: ['--no--sandbox'],
+        dumpio: false
+    })
+
+    const page = await browser.newPage();
+    await page.goto(url, {
+        waitUntil: 'networkidle2'
+    })
+
+    await sleep(3000)
+    await page.waitForSelector('.more')
+
+    for (let i = 0; i < 2; i++) {
+        await sleep(3000);
+        await page.click('.more')
+    }
+
+    const result = await page.evaluate(() => {
+        var $ = window.$;
+        var items = $('.list-wp a')
+        var links = []
+        if (items.length > 0) {
+            items.each((index, item) => {
+                let it = $(item)
+                let doubanId = it.find('div').data('id')
+                let title = it.find('.title').text()
+                let rate = Number(it.find('.rate').text())
+                let poster = it.find('img').attr('src').replace('s_ratio', 'l_radio')
+                links.push({
+                    doubanId,
+                    title,
+                    rate,
+                    poster
+                })
+            })
+        }
+        return links
+    })
+
+    browser.close();
+    console.log(result)
+})()
