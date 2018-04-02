387b29a8b862037faabe108538ec90278428be7c
guohjia <guohjia@gmail.com>
2018-04-02 16:00:18 +0800

    add koa tpl ,remove htmlwebpackplugin

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -4,14 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); //js/css文件填充 /
 const autoprefixer = require('autoprefixer'); //自动添加前缀,再看看用法
 const webpack = require('webpack');
 
+console.log('webpack  compiling...')
 module.exports = {
-    context: path.resolve(process.cwd(), "client"),    
+    context: path.resolve(process.cwd(), "../client"),    
     entry:{
         main:"./src",
         vendor: ['react', 'react-dom','antd']
     },
     output: {
-        publicPath: '/',
+        publicPath: '/static/',  //公共路径, 用来配置所有资源前面增加的路径,Maybe can be a cdn path
         path: path.resolve(process.cwd(), "/"),
         filename: "index.js",
     },
@@ -67,22 +68,25 @@ module.exports = {
         name:'vendor',
         filename: '[name].js'
       }),
-      new HtmlWebpackPlugin({
-        template: 'src/index.html',
-        //页面模板的地址, 支持一些特殊的模板, 比如jade, ejs, handlebar等
-        inject: true,
-        //文件插入的位置, 可以选择在 body 还是 head 中
-        hash: true,
-        //是否给页面的资源文件后面增加hash,防止读取缓存
-        minify: {
-            removeComments: true,
-            collapseWhitespace: false
-        },
-        //精简优化功能 去掉换行之类的
-        // chunks: ['vendor'],
-        //文件中插入的 entry 名称，注意必须在 entry 中有对应的申明，或者是使用 CommonsChunkPlugin 提取出来的 chunk. 简单理解即页面需要读取的js文件模块
-        filename: 'index.html'
-        //最终生成的 html 文件名称，其中可以带上路径名
-    })
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin() 
+    //   new webpack.optimize.UglifyJsPlugin() //生产环境
+    //   new HtmlWebpackPlugin({
+    //     template: 'src/index.html',
+    //     //页面模板的地址, 支持一些特殊的模板, 比如jade, ejs, handlebar等
+    //     inject: true,
+    //     //文件插入的位置, 可以选择在 body 还是 head 中
+    //     hash: true,
+    //     //是否给页面的资源文件后面增加hash,防止读取缓存
+    //     minify: {
+    //         removeComments: true,
+    //         collapseWhitespace: false
+    //     },
+    //     //精简优化功能 去掉换行之类的
+    //     // chunks: ['vendor'],
+    //     //文件中插入的 entry 名称，注意必须在 entry 中有对应的申明，或者是使用 CommonsChunkPlugin 提取出来的 chunk. 简单理解即页面需要读取的js文件模块
+    //     filename: 'index.html'
+    //     //最终生成的 html 文件名称，其中可以带上路径名
+    // })
     ]
 }
