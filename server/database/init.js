27382835d531eda0daaa783dd46ee8ba2d952d88
guohjia <guohjia@gmail.com>
2018-04-09 20:37:28 +0800

    数据库异常处理

--- server/database/init.js
+++ server/database/init.js
@@ -5,6 +5,7 @@ const db = 'mongodb://localhost/instagam';
 mongoose.Promise = global.Promise;
 
 exports.connect = () =>  {
+    let maxConnectTimes = 0;
     return new Promise((resolve,reject)=>{
         if (process.env.NODE_ENV !=='production'){
             mongoose.set('debug',true);
@@ -13,22 +14,26 @@ exports.connect = () =>  {
         mongoose.connect(db);
     
         mongoose.connection.on('disconnected', () => {
-            // mongoose.connect(db); //重新连接
+            if(maxConnectTimes < 5){
+                mongoose.connect(db); //重新连接
+            }else{
+                throw new Error('数据库出错,disconnected')
+            }
         })
     
         mongoose.connection.on('error', error => {
-            reject(error);
+            maxConnectTimes++;
+            if(maxConnectTimes < 5){
+                mongoose.connect(db); //重新连接
+            }else{
+                throw new Error('数据库出错了')
+            }
         })
     
         mongoose.connection.once('open', () => {
             resolve();
-        })
-    
-        
+        }) 
     }).then(()=>{
         console.log('MongoDB Connected Successfully')
-    },(err)=>{
-        console.log('数据库出错了')
-        console.log(err)
     })
 }
